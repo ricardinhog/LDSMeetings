d447ba0a62c9c785c575031cebafd18da0dd65fa
grosa82 <grosa82@gmail.com>
2015-08-17 08:40:29 -0600

    Add frequency controller

---
+++ OrganizadorReuniao/Controllers/FrequencyController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OrganizadorReuniao.Controllers
+{
+    public class FrequencyController : Controller
+    {
+        //
+        // GET: /Frequency/
+
+        public ActionResult Index(int frequencyType)
+        {
+
+
+
+
+
+            return View();
+        }
+
+    }
+}
