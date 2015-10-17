1b0d7029b381068726336f74e76016b45afe10bb
grosa82 <grosa82@gmail.com>
2015-10-16 19:20:43 -0600

    Add forgot my password feature

--- OrganizadorReuniao/Controllers/AccountController.cs
+++ OrganizadorReuniao/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using OrganizadorReuniao.Languages;
+﻿using OrganizadorReuniao.Helper;
+using OrganizadorReuniao.Languages;
 using OrganizadorReuniao.Models;
 using System;
 using System.Collections.Generic;
@@ -10,6 +11,39 @@ namespace OrganizadorReuniao.Controllers
 {
     public class AccountController : BaseController
     {
+        public ActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ForgotPassword(string email)
+        {
+            User user = new User().getUser(email.ToLower());
+            if (user.Id == 0)
+                return RedirectToAction("Feedback", new { message = "Usuário não encontrado" });
+            else
+            {
+                Common common = new Common();
+                string newPassword = common.generatePassword(6);
+                Result result = user.updateUser(user.Id, email.ToLower(), newPassword);
+                if (result.Success && common.sendEmail(email, string.Format("Sua nova senha é\n\n{0}\n\nAgenda SUD", newPassword), "Agenda SUD - Nova senha"))
+                {
+                    return RedirectToAction("Feedback", new { message = "Uma nova senha foi enviada para seu endereço de email" });
+                }
+                else
+                {
+                    return RedirectToAction("Feedback", new { message = "Erro ao enviar nova senha" });
+                }
+            }
+        }
+
+        public ActionResult Feedback(string message)
+        {
+            ViewBag.message = message;
+            return View();
+        }
+
         /// <summary>
         /// Create a new account
         /// </summary>
@@ -25,7 +59,7 @@ namespace OrganizadorReuniao.Controllers
             if (ModelState.IsValid)
             {
                 Models.User newUser = new Models.User();
-                
+
                 // check if the email address is already being used
                 if (!newUser.emailExists(model.Email))
                 {
@@ -71,7 +105,7 @@ namespace OrganizadorReuniao.Controllers
         {
             // remove validation errors
             foreach (var key in ModelState.Keys.ToList())
-                ModelState[key].Errors.Clear(); 
+                ModelState[key].Errors.Clear();
 
             if (model.Email != string.Empty && model.Password != string.Empty)
             {
@@ -142,15 +176,34 @@ namespace OrganizadorReuniao.Controllers
                     if (model.Password != string.Empty && model.Email != string.Empty)
                     {
                         Models.User user = new Models.User();
-                        Result result = user.updateUser(model.Id, model.Email, model.Password);
+                        string oldEmail = user.getUser(model.Id).Email;
+                        bool emailOk = false;
 
-                        if (result.Success)
-                            result = new Unit().updateUnit(model.UnitName, model.Id);
-
-                        if (result.Success)
+                        // check if the email id different
+                        if (oldEmail.ToLower() == model.Email.ToLower())
+                            emailOk = true;
+                        else
                         {
-                            Session["user"] = new User().getUser(model.Id);
-                            return RedirectToAction("UpdateSuccess");
+                            if (!user.emailExists(model.Email))
+                                emailOk = true;
+                        }
+
+                        if (emailOk)
+                        {
+                            Result result = user.updateUser(model.Id, model.Email, model.Password);
+
+                            if (result.Success)
+                                result = new Unit().updateUnit(model.UnitName, model.Id);
+
+                            if (result.Success)
+                            {
+                                Session["user"] = new User().getUser(model.Id);
+                                return RedirectToAction("UpdateSuccess");
+                            }
+                        }
+                        else
+                        {
+                            ModelState.AddModelError("", "Endereço de email já está sendo utilizado por outro usuário");
                         }
                     }
                     else
